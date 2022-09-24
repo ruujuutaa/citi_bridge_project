6f43688eef4601c069c8cfbebaddd0ebcfec97d2
ruujuutaa <palimkarrujuta@gmail.com>
2022-09-24 11:43:23 +0530

    Add files via upload

--- BackEnd/CitiBridge-1/src/main/java/com/citibridge/dao/AccountDAO.java
+++ BackEnd/CitiBridge-1/src/main/java/com/citibridge/dao/AccountDAO.java
@@ -4,5 +4,6 @@ import com.citibridge.entities.Account;
 
 public interface AccountDAO {
 	public int login(String username, String password);
-
+	public int addStocks(String stock, String username);
+	public int removeStock(String stock, String username);
 }
--- BackEnd/CitiBridge-1/src/main/java/com/citibridge/dao/AccountDAOImpl.java
+++ BackEnd/CitiBridge-1/src/main/java/com/citibridge/dao/AccountDAOImpl.java
@@ -2,12 +2,14 @@ package com.citibridge.dao;
 
 import org.springframework.stereotype.Repository;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 
 import com.citibridge.entities.Account;
 @Repository("accountDAO")
@@ -29,6 +31,28 @@ public class AccountDAOImpl implements AccountDAO {
 			else
 				return 0;
 				
+	}
+	@Override
+	public int addStocks(String stock, String username) {
+		Query query = new Query(Criteria.where("username").is(username));
+		ArrayList<String> stocks = new ArrayList<String>();
+		String[] prevStocks = mongoTemplate.findOne(query, Account.class).getStocks();
+		stocks.addAll(Arrays.asList(prevStocks));
+		stocks.add(stock);
+		mongoTemplate.updateFirst(query,Update.update("stocks", stocks.toArray()), Account.class);	
+		return 1;
+	}
+	@Override
+	public int removeStock(String stock, String username) {
+		// TODO Auto-generated method stub
+		Query query = new Query(Criteria.where("username").is(username));
+		ArrayList<String> stocks = new ArrayList<String>();
+		String[] prevStocks = mongoTemplate.findOne(query, Account.class).getStocks();
+		stocks.addAll(Arrays.asList(prevStocks));
+		stocks.remove(stock);
+		mongoTemplate.updateFirst(query,Update.update("stocks", stocks.toArray()), Account.class);	
+		return 1;
+		
 	}
 
 }
