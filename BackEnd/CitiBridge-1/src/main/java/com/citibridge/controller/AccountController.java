06b4484bd609b143af6baa220d49d20704607d2a
ruujuutaa <palimkarrujuta@gmail.com>
2022-09-24 11:42:27 +0530

    Add files via upload

--- BackEnd/CitiBridge-1/src/main/java/com/citibridge/controller/AccountController.java
+++ BackEnd/CitiBridge-1/src/main/java/com/citibridge/controller/AccountController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 //import org.springframework.ui.ModelMap;
 //import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.citibridge.entities.Account;
@@ -37,6 +39,21 @@ public class AccountController {
 			return accountService.login(account.getUserName(), account.getPassword());
 			
 		
+	}
+	@GetMapping("/update/{stock}/{username}")
+	@ResponseBody
+	public int addStocks(@PathVariable("stock") String stock, @PathVariable("username") String username) 
+	{
+		System.out.println(stock + username);
+		return accountService.addStocks(stock,username);
+	}
+	
+	@GetMapping("/remove/{stock}/{username}")
+	@ResponseBody
+	public int removeStock(@PathVariable("stock") String stock, @PathVariable("username") String username) 
+	{
+		System.out.println(stock + username);
+		return accountService.removeStock(stock,username);
 	}
 	
 	
