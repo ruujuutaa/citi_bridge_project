17b74d1a684babf6f68ae40e27d79799ffa91b57
ruujuutaa <palimkarrujuta@gmail.com>
2022-09-24 15:50:56 +0530

    Add files via upload

--- BackEnd/CitiBridge-1/src/main/java/com/citibridge/service/AccountService.java
+++ BackEnd/CitiBridge-1/src/main/java/com/citibridge/service/AccountService.java
@@ -2,8 +2,12 @@ package com.citibridge.service;
 
 import com.citibridge.entities.Account;
 
+import java.util.List;
+
+import com.citibridge.entities.*;
 public interface AccountService {
 	public int login(String username, String password);
 	public int addStocks(String stock, String username);
 	public int removeStock(String stock, String username);
+	public List<StockWrapper> getSavedStocks(String username);
 }
--- BackEnd/CitiBridge-1/src/main/java/com/citibridge/service/AccountServiceImpl.java
+++ BackEnd/CitiBridge-1/src/main/java/com/citibridge/service/AccountServiceImpl.java
@@ -1,10 +1,13 @@
 package com.citibridge.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.citibridge.dao.AccountDAO;
 import com.citibridge.entities.Account;
+import com.citibridge.entities.StockWrapper;
 @Service("accountService")
 public class AccountServiceImpl implements AccountService {
 	@Autowired
@@ -23,6 +26,11 @@ public class AccountServiceImpl implements AccountService {
 	public int removeStock(String stock, String username) {
 		// TODO Auto-generated method stub
 		return accountDAO.removeStock(stock,username);
+	}
+	@Override
+	public List<StockWrapper> getSavedStocks(String username) {
+		// TODO Auto-generated method stub
+		return accountDAO.getSavedStocks(username);
 	}
 
 }
